60fe7b1b7b150861d8c170a973f54f73cbb74603
Laura Worboys <65424945+pacificPlumeria@users.noreply.github.com>
2020-11-18 00:22:04 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import SignUp from './components/signUp';
 import HomePage from './components/home.js'
+import SignOut from './components/signOut';
+import contactPage from './components/contactPage';
+import TestResults from './components/testResult';
+import DailySymptoms from './components/dailySx';
 
 
 
@@ -17,15 +21,19 @@ import HomePage from './components/home.js'
  function App() {
   return(
     <Router>
-     <div className="container">
+      <div className="container">
+        <Route path="/signUp"  component={SignUp} /> 
+        <Route path="/"  exact component={Login} /> 
+        <Navbar />
+          <Switch>
 
-      <Navbar />
-      <Switch>
-      <Route path="/sign*"  component={SignUp} /> 
-     <Route path="/log*"  component={Login} /> 
-      <Route path="/home" component={HomePage} /> 
-        <Route path="/cont*" component={ContactInfo} /> 
-      </Switch>
+            <Route path="/home" component={HomePage} /> 
+            <Route path="/contactPage" component={contactPage} /> 
+            <Route path="/contactInfo" component={ContactInfo} /> 
+            <Route path="/testResult" component={TestResults} /> 
+            <Route path="/dailySx" component={DailySymptoms} /> 
+            <Route path="/signOut"  component={SignOut} /> 
+         </Switch>
       </div>
     </Router>
   );
